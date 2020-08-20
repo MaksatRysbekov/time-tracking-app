376d25992e183b90da0ac567903584b22d122dc2
Maksat Rysbekov <maksatrysbekov96@gmail.com>
2020-08-20 16:08:31 +0200

    first commit

---
+++ src/main/java/com/makdev/timetrackkingapp/TimetrackingappApplication.java
@@ -0,0 +1,13 @@
+package com.makdev.timetrackkingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetrackingappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimetrackingappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/makdev/timetrackkingapp/TimetrackkingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makdev.timetrackkingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrackkingappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
